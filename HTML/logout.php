7d4262645bc2d1a1f860908f49c1dad09c4567d2
Himanshu Bakre <139998522+himanshubakre@users.noreply.github.com>
2024-02-10 21:47:01 +0530

    Delete HTML directory

--- HTML/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['user']['name']);
- 	unset($_SESSION['user']['id']);
-unset($_SESSION['user']['email']);
-
-  echo '<script>alert("Logout Successfull") </script>';
-     echo "<script>window.location.href='login.html'</script>";
-?>
